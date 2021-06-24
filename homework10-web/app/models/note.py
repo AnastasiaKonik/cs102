9fe8bed151d044beebae1696d078129a5e1469ae
Anastasiia Konik <konik.ftl@mail.ru>
2021-06-24 15:25:39 +0300

    HW 10 is done now

---
+++ homework10-web/app/models/note.py
@@ -0,0 +1,20 @@
+from tortoise import fields
+
+from app.models.base import TimestampedMixin
+from app.models.user import User
+
+
+class Note(TimestampedMixin):
+    text = fields.TextField(null=False)
+    author: fields.ForeignKeyRelation[User] = fields.ForeignKeyField(
+        "models.User", to_field="id"
+    )
+
+    def __repr__(self):
+        return f"Nore(text='{self.text}', author='{self.author}')"
+
+    def to_dict(self):
+        user_dict = self.__dict__.copy()
+        user_dict.update({"_author": None})
+
+        return user_dict
---
+++ homework10-web/app/services/db.py
@@ -0,0 +1,43 @@
+import traceback
+
+from fastapi.logger import logger
+from tortoise import Tortoise
+from tortoise.backends.base.config_generator import generate_config
+
+from app import app
+from app.services import settings
+
+tortoise_config: dict = generate_config(
+    settings.POSTGRES_URI, {"models": ["app.models"]}
+)
+
+
+def describe_credentials():
+    described_models: dict = tortoise_config
+    for database in described_models.get("connections"):
+        source_pass = settings.POSTGRES_PASSWORD
+        if len(source_pass) > 0:
+            hided_pass = source_pass[0] + "*" * len(source_pass[1:-1]) + source_pass[-1]
+            described_models["connections"][database]["credentials"][
+                "password"
+            ] = hided_pass
+
+    return described_models
+
+
+@app.on_event("startup")
+async def on_startup():
+    try:
+        await Tortoise.init(
+            config=tortoise_config,
+        )
+        await Tortoise.generate_schemas()
+        logger.info("PostgreSQL Connection opened")
+        logger.info(describe_credentials())
+    except Exception as e:
+        traceback.print_exc()
+
+
+@app.on_event("shutdown")
+async def on_shutdown():
+    await Tortoise.close_connections()
